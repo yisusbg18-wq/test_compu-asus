0956dfc563d0436f89c3fc19e20961a342824535
yisusbg18-wq <yisusbg18@gmail.com>
2026-05-28 08:46:59 -0600

    learning_python/understabding_strings.py

---
+++ learning_python/understabding_strings.py
@@ -0,0 +1,34 @@
+' esto es un string'
+subject_name = "clase de programacion" # (Añadí la 'a' que faltaba en programación 😉)
+print(subject_name.title())
+
+# un metodo es una ccion qu python puede realizar en un fragmento de datos o sobre una varibale.
+# el punto (.) despues de una varibale seguido del metodo (palabra) title() dice que se tine que ejecutar el motodo de title() de la varibale subject name
+# Todos los metodos van seguidos de parentesis por que en ocasiones necesitan informacion adicional para funcionar, lo cual iria dentro de los parentesis
+# en esta ocasion el medoto .title() no requiere informacion adicinal para ejecutarse
+
+print("metodo upper")
+print(subject_name.upper()) #conviertea mayusculas todas las lettras
+print("METODO LOWER".lower())
+print(subject_name.lower()) #convierte a minusculas todas la letra
+
+'CONCATENACION O COMBINACION DE STRING'
+'Nota: la cocantenacion solo puede realizar con string'
+
+print("\n\n")
+print("concatenacion de string")
+first_name = "yo merenges"
+last_name = "mercury"
+full_name = first_name 
+# haz lo que te falta
+
+
+'whitespaces, se refiere a cualquier a cualquier caracter que no se imprime, es decir, un espacio, tabuladores y finales de linea. Los whitespaces se utilizan comunmente para oraganizar las salidas, del tal manera que sean mas amaigable de ver y leer para el usuario
+
+
+#tabulador
+print("WHITESPACES")
+print("python")
+print("\tpython")
+
+#saldo de liner
